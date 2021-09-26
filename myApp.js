5641baba4ab97b63d756efb5945846911d7ba1c7
AkshatParmar <Akshat.parmar@rutgers.edu>
2021-09-26 16:31:46 -0400

    Challenge #5

--- myApp.js
+++ myApp.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var app = express();
 
-// Challenge #1
-console.log("Hello World")
+// Challenge #4
+// app.use(express.static(__dirname,"/public"));
+
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-	console.log("Yo");
+	console.log(__dirname);
 	res.sendFile(__dirname + '/views/index.html');
 })
 
-
+// Challenge #5
+app.get("/json", (req, res) => {
+	const result = new Object();
+	result["message"] = "Hello json";
+	res.json(result);
+});
 
 
 
